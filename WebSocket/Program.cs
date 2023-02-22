8f33d5b05bb3e7101508f206990c197ef0f8172c
igor-miranda <igor.miranda@ufvjm.edu.br>
2023-02-22 14:52:11 -0300

    WebSocket

---
+++ WebSocket/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.WebSockets;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        // cria o URI do WebSocket
+        Uri uri = new Uri("wss://demo.piesocket.com/v3/channel_123?api_key=VCXCEuvhGcBDP7XhiJJUDvR1e1D3eiVjgZ9VRiaV&notify_self");
+        
+        // cria um cliente WebSocket
+        using (ClientWebSocket client = new ClientWebSocket())
+        {
+            // conecta ao servidor WebSocket
+            await client.ConnectAsync(uri, CancellationToken.None);
+
+            // envia uma mensagem ao servidor WebSocket
+            Console.WriteLine("Conectado ao servidor WebSocket!");
+            string message = "Olá, servidor WebSocket!";
+            byte[] messageBytes = System.Text.Encoding.UTF8.GetBytes(message);
+            await client.SendAsync(new ArraySegment<byte>(messageBytes), WebSocketMessageType.Text, true, CancellationToken.None);
+
+            // aguarda mensagens do servidor WebSocket
+            byte[] buffer = new byte[1024];
+            while (true)
+            {
+                WebSocketReceiveResult result = await client.ReceiveAsync(new ArraySegment<byte>(buffer), CancellationToken.None);
+
+                if (result.MessageType == WebSocketMessageType.Text)
+                {
+                    string receivedMessage = System.Text.Encoding.UTF8.GetString(buffer, 0, result.Count);
+                    Console.WriteLine("Mensagem recebida do servidor WebSocket: {0}", receivedMessage);
+                }
+            }
+        }
+    }
+}
+
+
